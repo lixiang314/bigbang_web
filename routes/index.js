207e118121353c4fdb4a341b9420032e79f1ae1a
lixzap <lixzap@gmail.com>
2016-10-26 15:14:49 +0800

    base function ajax

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
-var router = express.Router();
+var Segment = require('segment');
 
+var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+
+  res.render('index', {});
+
 });
+
+
+router.post('/ajax/split', function(req, res, next) {
+  console.log('分词');
+  var segment = new Segment();
+  segment.useDefault();
+
+  var result = segment.doSegment(req.body.textStr, {
+    simple: true,
+  });
+  res.send({textArr:result})
+});
+
+
 
 module.exports = router;
